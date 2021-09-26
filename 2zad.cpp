2fdd9020f014dc5d50189cbc9969faeada212831
bZyulyamova <90328938+bZyulyamova@users.noreply.github.com>
2021-09-26 13:42:35 +0300

    new source file
    add C file

---
+++ 2zad.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char dayAsc[7][20] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    char dayDes[7][20] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    int year, sum = 0, i = 2013;
+    scanf("%d", &year);
+    if(year > 2013)
+    {
+            for(i = 2014; i <= year; i++)
+             if(i % 4 == 0) sum = (sum + 2) % 7;
+             else sum = (sum + 1) % 7;
+            printf("%s\n", dayAsc[sum]);
+     }
+    else
+    {
+        for(i; i > year; i--)
+        if(i % 4 == 0) sum = (sum + 2) % 7;
+        else sum = (sum + 1) % 7;
+        printf("%s\n", dayDes[6 - sum]);
+    }
+    system("pause");
+    return 0;
+}
+ 
