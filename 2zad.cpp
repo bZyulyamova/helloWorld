408a2467424d4a4c11174956c3920afc6164a650
bZyulyamova <90328938+bZyulyamova@users.noreply.github.com>
2021-09-29 08:37:05 +0300

    Update 2zad.cpp

--- 2zad.cpp
+++ 2zad.cpp
@@ -19,6 +19,8 @@ int main()
         if(i % 4 == 0) sum = (sum + 2) % 7;
         else sum = (sum + 1) % 7;
         printf("%s\n", dayDes[6 - sum]);
+        printf("\n");
+        printf("\n");
     }
     system("pause");
     return 0;
