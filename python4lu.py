04fa74e9b1b848323e39e332b62e0303bf63b5f9
bZyulyamova <90328938+bZyulyamova@users.noreply.github.com>
2021-10-26 08:52:04 +0300

    new code line

--- python4lu.py
+++ python4lu.py
@@ -1,5 +1,7 @@
 #2 zad
+from random import randint
 count = int(input("Enter count of number: "))
+first_list = [randint(1, 20) for i in range(count)]
 print(first_list)
 '''for k in range(0, count+2,2):
     first_list.insert(k+1, first_list[k]+first_list[k+1])
